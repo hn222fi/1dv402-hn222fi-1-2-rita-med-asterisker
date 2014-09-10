4c6cc964b99992bcd1ef7065d3a56b8163222b76
Henrik Nilsson <hn222fi@student.lnu.se>
2014-09-10 13:31:02 +0200

    Initierat metoder och beskrivit dem samt skapat resursfilen

--- 1-2-rita-med-asterisker/1-2-rita-med-asterisker/Program.cs
+++ 1-2-rita-med-asterisker/1-2-rita-med-asterisker/Program.cs
@@ -8,8 +8,45 @@ namespace _1_2_rita_med_asterisker
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+        }
+        /// <summary>
+        /// Kollar om användaren vill avsluta programmet eller fortsätta köra programmet en gång till
+        /// </summary>
+        /// <returns>Ett boolean värde</returns>
+        private static bool IsContinuing()
+        {
+            return true;
+        }
+        /// <summary>
+        /// Ska läsa in ett udda heltal av typen byte från användaren
+        /// </summary>
+        /// <param name="prompt">Felmeddelande som ska visas om användaren visar fel värde</param>
+        /// <param name="maxValue">Maxvärde som användaren kan mata in</param>
+        /// <returns>Ett udda heltal av typen byte</returns>
+        private static byte ReadOddByte(String prompt=null,byte maxValue = 255)
+        {
+            return 255;
+        }
+        /// <summary>
+        /// Ritar ut diamanten
+        /// </summary>
+        /// <param name="maxCount">Ger antalet asterisker diamentens midja ska ha</param>
+        private static void RenderDiamond(byte maxCount)
+        {
+
+        }
+        /// <summary>
+        /// Ritar ut en rad i diamanten
+        /// </summary>
+        /// <param name="maxCount">Midjans läng (längsta raden asterisker)</param>
+        /// <param name="asteriskCount">Längden på raden</param>
+        private static void RenderRow(int maxCount,int asteriskCount)
+        {
+
         }
     }
 }
